46bac311cff86686750e7d0edf75614d5544273a
Luan de Souza <luandesouzanascimento2011@gmail.com>
2022-01-16 19:20:20 -0300

    refact: svg in public folder

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { AdjustmentsIcon } from '@heroicons/react/solid';
 import Link from 'next/link';
 import { UserContext } from '../context/userContext';
+import Image from 'next/image';
 
 const Header = () => {
   const { accessToken, logout } = React.useContext(UserContext);
@@ -12,10 +13,12 @@ const Header = () => {
         <Link href={accessToken ? '/perguntas' : '/'}>
           <a className="flex items-center text-black font-bold">
             <h2 className="text-4xl" >Ágil </h2>
-            <svg width="38" height="32" viewBox="0 0 38 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M1 30.5C10 34.5 10.5 6.49999 29.5 7.99997" stroke="black" stroke-width="1.75" stroke-linecap="round"/>
-              <path d="M4 27C7.5 23.5 4 15.5 10.5 8.00004C17 0.500049 39 1.00005 36 3.00005C33 5.00004 34.5 18.5 23.5 25C12.5 31.5 14 24 10.5 27" stroke="black" stroke-width="1.75" stroke-linecap="round"/>
-            </svg>
+            <Image
+              src="/leaf.svg"
+              width={50}
+              height={50}
+              objectFit="contain"
+            />
           </a>
         </Link>
       </div>
