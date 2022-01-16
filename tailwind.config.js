bc37ef49ef37f9c67c005965da885299276942a8
Luan de Souza <luandesouzanascimento2011@gmail.com>
2022-01-16 18:22:19 -0300

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      'primary': '#558B2F',
+      'primary-dark': '#1B5E20',
+      'primary-light': '#97B460',
+      'primary-lighteen': '#EBE6D3',
+      'secondary': '#1E88E5',
+      'secondary-dark': '#1565C0',
+      'secondary-light': '#42A5F5',
+      'secondary-lighteen': '#BBDEFB',
+      'black': '#212121',
+      'dark-gray': '#323232',
+      'gray': '#626262',
+      'light-gray': '#BDBDBD',
+      'white': '#FFFFFF'
+    },
     extend: {},
   },
   plugins: [],
