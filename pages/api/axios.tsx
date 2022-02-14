32000d3238936d3ac7d412f0656833fb33444578
haurenburu <haurenburu@protonmail.com>
2022-02-14 13:12:05 +0000

    final commit

--- pages/api/axios.tsx
+++ pages/api/axios.tsx
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const base_url = 'http://127.0.0.1:3000';
+const base_url = 'http://45.79.253.74:3000';
 let accessToken: string;
 
 if (typeof window !== 'undefined') {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Header from '../components/header';
 
 export default function Home() {
@@ -8,7 +9,25 @@ export default function Home() {
         <title>Agil-PA2</title>
       </Head>
       <Header />
-      INDEX
+      <div className="flex justify-around flex-col items-center">
+        <div className="w-full max-w-lg">
+          <Image
+            src="/home.svg"
+            width="100%"
+            height="100%"
+            layout="responsive"
+            objectFit="contain"
+          />
+        </div>
+        <div className="container text-center">
+          <p className="p-5 text-2xl font-bold text-teal-800">
+            Um espaço para produtores rurais tirarem suas dúvidas! Especialistas
+            de diversas áreas, reunidos para ajudar os pequenos e médios
+            produtores. Tudo On-line!
+          </p>
+        </div>
+      </div>
     </>
   );
 }
+
